44d5cced4b57dd991d718be02c9cced9a68acc98
Virat Singh <114825043+viratsingh24@users.noreply.github.com>
2024-01-13 23:03:27 +0530

    Add files via upload

---
+++ variable1.java
@@ -0,0 +1,16 @@
+public class variable {
+    public static void main (String args[]){
+        //variables
+        String name = "virat";
+        int age = 21;
+        int a = 24;
+        int b = 45;
+        double price = 78.87;
+       // System.out.println(a);
+        int diffee = b-a;
+        System.out.println(diffee);
+
+
+    }
+    
+}
